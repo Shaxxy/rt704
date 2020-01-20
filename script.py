4ff3d988e9174b95169bdd6ff76828b3779e8419
Shaxxy <59960429+Shaxxy@users.noreply.github.com>
2020-01-20 09:21:08 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import random
+
+#TACHE 1
+num = random.randint(0,10)
+#print(num)
+
+num2 = random.randint(0,10)
+#print(num2)
+
+total1 = num + num2
+print("Result task 1:", total1)
+
+#TACHE 2
+num = random.randint(0,10)
+#print(num)
+
+num2 = random.randint(0,10)
+#print(num2)
+
+total2 = num - num2
+print("Result task 2:", total2)
